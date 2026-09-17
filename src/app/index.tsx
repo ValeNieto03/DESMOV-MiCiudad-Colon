bd5375120fc49f2897b2e3f3491e56548c1b5b6d
ValeNieto03 <valentelinanieto@gmail.com>
2026-09-16 22:09:25 -0300

    Agrega mapa OSM y marcador de Termas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
 
 export default function HomeScreen() {
   return (
@@ -26,19 +27,63 @@ export default function HomeScreen() {
           <Text style={styles.searchText}>¿Qué querés conocer?</Text>
         </Pressable>
 
-        {/* Mapa */}
         <View style={styles.mapCard}>
-          <View style={styles.mapBackground}>
-            <Text style={styles.mapEmoji}>📍</Text>
-            <Text style={styles.mapTitle}>Mapa de Colón</Text>
-            <Text style={styles.mapSubtitle}>
-              Lugares turísticos cerca tuyo
-            </Text>
+          <WebView
+            originWhitelist={['*']}
+            source={{
+              html: `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            />
 
-            <Pressable style={styles.mapButton}>
-              <Text style={styles.mapButtonText}>Ver mapa</Text>
-            </Pressable>
-          </View>
+            <link
+              rel="stylesheet"
+              href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+            />
+
+            <style>
+              html, body, #map {
+                height: 100%;
+                width: 100%;
+                margin: 0;
+                padding: 0;
+              }
+            </style>
+          </head>
+
+          <body>
+            <div id="map"></div>
+
+            <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+            <script>
+              const map = L.map('map').setView(
+                [-32.22518, -58.13802],
+                14
+              );
+
+              L.tileLayer(
+                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                {
+                  attribution: '&copy; OpenStreetMap contributors'
+                }
+              ).addTo(map);
+
+              L.marker([-32.20895, -58.14675])
+              .addTo(map)
+              .bindPopup('<b>Termas de Colón</b>')
+              .openPopup();
+            </script>
+          </body>
+        </html>
+      `,
+            }}
+            style={styles.map}
+          />
         </View>
 
         {/* Lugares cercanos */}
@@ -160,6 +205,10 @@ const styles = StyleSheet.create({
     borderRadius: 22,
     overflow: 'hidden',
     marginBottom: 22,
+  },
+
+  map: {
+    flex: 1,
   },
 
   mapBackground: {
